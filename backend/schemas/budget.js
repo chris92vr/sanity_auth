100d9b3760ce2c751db4b6422661ec0a02c934c5
chris92vr <garofolichristian@gmail.com>
2022-07-29 23:38:04 +0200

    Added model budget & expense

---
+++ backend/schemas/budget.js
@@ -0,0 +1,26 @@
+export default {
+  name: 'budget',
+  title: 'Budget',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'max',
+      title: 'Max',
+      type: 'number',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'user_id',
+      title: 'User',
+      type: 'reference',
+      to: ['user'],
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
---
+++ backend/schemas/expense.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'expense',
+  title: 'Expense',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'amount',
+      title: 'Amount',
+      type: 'number',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'budget_id',
+      title: 'Budget',
+      type: 'reference',
+      to: ['budget'],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'createdAt',
+      title: 'CreatedAt',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
